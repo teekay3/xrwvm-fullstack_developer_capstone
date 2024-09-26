4b83593606b74ba975fc44c176d8515e4634bbb6
teekay3 <thulanieknene@gmail.com>
2024-09-26 09:39:37 -0400

    post review changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import PostReview from "./components/Dealers/PostReview"
 import { Routes, Route } from "react-router-dom";
 
